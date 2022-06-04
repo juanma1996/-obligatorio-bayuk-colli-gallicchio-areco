7c451f6fbec1734f7aee350173a8362578c12518
carlos_manya <c.colli1981@gmail.com>
2022-06-04 16:11:57 -0300

    Add files to gitIgnore

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,20 +4,11 @@ require('@nomiclabs/hardhat-ethers');
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
 module.exports = {
-  solidity: "0.7.3",
+  solidity: "0.8.9",
   networks: {
-    rinkeby: {
-      chainId: 4,
-      timeout: 20000,
-      gasPrice: 8000000000,
-      gas: "auto",
-      name: "Rinkeby",
-      url: process.env.RINKEBY_ACCESSPOINT_URL,
-      from: process.env.RINKEBY_ACCOUNT,
-      accounts: [process.env.RINKEBY_PRIVATE_KEY]
-    },
-    ganache: {
+      ganache: {
       chainId: 1337,
       timeout: 20000,
       gasPrice: 8000000000,
@@ -26,12 +17,13 @@ module.exports = {
       url: process.env.GANACHE_URL,
       from: process.env.GANACHE_ACCOUNT,
       accounts: [process.env.GANACHE_PRIVATE_KEY]
+
     },
     hardhat:{
       chainId: 31337,
       name: "hardhat",
       gas: "auto",
-      gasprice: "auto"
+      gasPrice: "auto"
     }
   }
 };
