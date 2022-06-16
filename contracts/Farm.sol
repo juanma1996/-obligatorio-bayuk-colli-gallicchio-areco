9173588363c1840ddd75216a8af870629b4d3a38
paula <pauareco@gmail.com>
2022-06-16 15:57:29 -0300

    changed farm structure and implemented most methods

--- contracts/Farm.sol
+++ contracts/Farm.sol
@@ -6,82 +6,93 @@ pragma solidity 0.8.14;
 //When unstaking, we have to make sure the unstaking amount is equal or more than the users staked balance. 
 
 contract Farm {
-
     uint256 private totalStake;
-    uint8 private APR;
+    uint256 private APR;
+    address private _vaultContract;
 
     struct Stake{
-        address userAddress; 
         uint256 amount;
         uint256 since;
+        uint256 yield; //increases next time the user stakes
     }
 
-    struct Stakeholder{
-        address userAddress;
-        Stake[] userAddressStakes;
-    }
-    
-    Stakeholder[] internal stakeholders;
-
-    mapping(address => Stake) private _stakeBalancesByAddress;
-    mapping(address => uint256) private _stakeBalancesIndex;
+    mapping(address => Stake) private _stakes;
 
     constructor(uint256  _total, uint8  _APRval){
         totalStake = _total;
         APR = _APRval;
     }
 
-    function APRfunction() public view returns (uint8)
+    function setVaultContract() external
     {
-        return APR;
+         _vaultContract = msg.sender;
     }
 
-    //the index is the space in the array in which the users stakes are stored, when we have an address, the stake
-    //index can be retrieved by using mapping _stakeBalancesIndex
-    //StakeEvent is emitted when an user stakes any amount >0.
-    event StakeEvent(address indexed userAddress, uint256 amount, uint256 index, uint256 stakedSince);
-
-    //first we have to add the stakeholder, this means creating a space in the stakeholders array
-    function _addStakeholder(address newStakeholder) internal returns (uint256){
-        stakeholders.push(); //create empty space
-        uint256 indexIs = stakeholders.length - 1; //calculates the index number
-        stakeholders[indexIs].userAddress = newStakeholder; //assigns the address to the index created
-        _stakeBalancesIndex[newStakeholder]; //adds index to the stakeholder
-        return indexIs;
+    function setAPRfunction() public view returns (uint256)
+    {
+        require(msg.sender == _vaultContract);
+        return APR;
     }
 
      //stake(uint256 _amount)
     function _stake(uint256 _amount) internal{
         require(_amount > 0, "Cannot stake zero.");
-        
-        uint256 index = _stakeBalancesIndex[msg.sender];
-        uint256 stakedSince = block.timestamp;
 
-        if(index == 0){ //if its the first time this person is staking, we add them to the stakeholders collection
-            index = _addStakeholder(msg.sender); //addStakeholders returns the index assigned to the person
-        }
+        _stakes[msg.sender].amount += _amount;
+        _stakes[msg.sender].since = block.timestamp;
+        _stakes[msg.sender].yield = calculateYield();
 
         totalStake += _amount;
 
-        stakeholders[index].userAddressStakes.push(Stake(msg.sender, _amount, stakedSince)); //pushing the new stake
-        emit StakeEvent(msg.sender, _amount, index,stakedSince);
+        // emit StakeEvent(msg.sender, _amount, index,stakedSince);
+    }
+
+    function calculateYield() internal view returns (uint256){
+        uint256 today = block.timestamp;
+        uint256 diff = (today - _stakes[msg.sender].since) / 1 days; // / 60 / 60 / 24; //diff in days 
+        uint256 updatedYield = diff * APR / 365; //APR is in 365 days
+
+        return updatedYield;
     }
 
     //unstake(uint256 _amount)
+    function unstake(uint256 _amount) internal{
+         require(_amount <= _stakes[msg.sender].amount, "Cannot stake more than stake amount.");
+
+        _stakes[msg.sender].amount -= _amount;
+        //_stakes[msg.sender].since = block.timestamp; //TODO: WHAT HAPPENS TO 'SINCE' WHEN UNSTAKING ?
+
+        totalStake -= _amount;
+    }
 
     //withdrawYield()
+    function withdrawYield() public returns (uint256) {
+        calculateYield(); //because yield gets calculated the next stake a person makes, so its not up to date now
+        _stakes[msg.sender].since = block.timestamp; //as yield is not up to date, we had to calculate yield, to not calculate it twice next time we are staking or withdrowingwe update "since"
+        uint256 toReturn = getYield();
+        resetYield();
+        return toReturn;
+    }
 
-    //getYield()
+    function resetYield() internal{
+        _stakes[msg.sender].yield = 0;
+    }
+
+    //getYield() 
+    function getYield() public view returns (uint256) {
+       return _stakes[msg.sender].yield; 
+    }
+
 
     //getStake()
     function getStake() public view returns (uint256) {
-       // return address(msg.sender);
+       return totalStake; 
     }
 
-    function getBalance(address _owner) public view returns (uint256 balance)
-    {
-        return _stakeBalancesByAddress[_owner].amount;
-    }
+    // function getBalance(address _owner) public view returns (uint256 balance)
+    // {
+    //     // return _stakeBalancesByAddress[_owner].amount;
+    // }
 
     //getTotalStake()
     function getTotalStake() public view returns (uint256) {
@@ -91,7 +102,15 @@ contract Farm {
     //getTotalYieldPaid()
 
     //getAPR()
+    function getAPR() public view returns (uint256){
+        return APR;
+    }
 
     //setAPR(uint256 _value)
+    function setAPR(uint256 _value) external returns (bool) { //PRE: Setter is admin
+        require(msg.sender == _vaultContract);
+        APR = _value;
+        return true;
+    }
 
 }
