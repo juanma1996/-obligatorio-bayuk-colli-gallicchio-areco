835b07e16587d9a7de3f36060534c675a09f4793
Agustin Bayuk <agustin.bayuk@mercadolibre.com>
2022-06-18 23:17:54 -0300

    maxAmountToTransfer

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.14;
 contract Vault{
 
     address private tokenContract;
+    uint256 private maxAmountToTransfer;
 
     constructor(){}
 
@@ -15,5 +16,12 @@ contract Vault{
         bytes memory mintToken = abi.encodeWithSignature("mint(uint256)", amount);
         (bool _success, bytes memory _returnData) = tokenContract.call(mintToken);
         return _success;
+    }
+
+    function setMaxAmountToTransfer(uint256 maxAmount) external {
+        require(maxAmount > 0, "Max amount should be a positive number");
+        require(maxAmount < 2^256, "Max amount should be minor than uint limit");
+        //checkear si es admin
+        maxAmountToTransfer = maxAmount;
     }
 }
